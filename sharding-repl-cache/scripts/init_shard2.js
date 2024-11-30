4fab9f88a9c0fcf6be13a4d8f7f5e8eed254b75d
BlackDEK <BlackDEK546@yandex.ru>
2024-11-30 17:27:52 +0500

    - Добавлен вариант с кэшированием.

---
+++ sharding-repl-cache/scripts/init_shard2.js
@@ -0,0 +1,9 @@
+rs.initiate({
+  _id: "shard2",
+  members: [
+    { _id: 0, host: "shard2:27019" },
+    { _id: 1, host: "shard2Replica1:27119" },
+    { _id: 2, host: "shard2Replica2:27219" },
+  ],
+});
+exit();
